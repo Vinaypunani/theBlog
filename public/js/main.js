d8093253f548fed02f58eb74d19e9dda27a50e88
vinaypunani <vinaypunani@gmail.com>
2024-06-01 13:37:51 +0530

    Commit Web

---
+++ public/js/main.js
@@ -0,0 +1,14 @@
+// const on = document.querySelector('.swap-on');
+// const off = document.querySelector('.swap-off');
+
+// on.addEventListener('click',()=>{
+
+// })
+
+
+const chackbox = document.querySelector('.checkbox');
+const blog = document.querySelector('.theblog');
+
+if(chackbox.checked){
+    console.log(blog.attributes)
+}
